deca9cde144f162b647e548371048e913efa5f76
osfa <jacob.broms@prototyp.se>
2021-09-21 09:26:04 +0200

    init

---
+++ src/main.js
@@ -0,0 +1,75 @@
+// This is the main.js file. Import global CSS and scripts here.
+// The Client API can be used here. Learn more: gridsome.org/docs/client-api
+
+// v2.0
+import Vuetify from 'vuetify'
+import 'vuetify/dist/vuetify.min.css'
+import DefaultLayout from '~/layouts/Default.vue'
+
+import colors from 'vuetify/lib/util/colors'
+import * as VueGoogleMaps from 'vue2-google-maps'
+
+export default function (Vue, { appOptions, head }) {
+  head.link.push({
+    rel: 'stylesheet',
+    href: 'https://cdn.jsdelivr.net/npm/@mdi/font@latest/css/materialdesignicons.min.css',
+  })
+  
+  head.link.push({
+    rel: 'stylesheet',
+    href: 'https://fonts.googleapis.com/css?family=Roboto:100,300,400,500,700,900',
+  });
+  
+  const opts = { 
+    theme: {
+      themes: {
+        light: {
+          primary: colors.purple,
+          secondary: colors.grey.darken1,
+          accent: colors.shades.black,
+          error: colors.red.accent3,
+        },
+        dark: {
+          primary: colors.blue.lighten3,
+        },
+      },
+    }
+  }//opts includes, vuetify themes, icons, etc.
+  
+  Vue.use(Vuetify)
+  
+  Vue.use(VueGoogleMaps, {
+    load: {
+      key: 'YOUR_API_TOKEN',
+      libraries: 'places', // This is required if you use the Autocomplete plugin
+      // OR: libraries: 'places,drawing'
+      // OR: libraries: 'places,drawing,visualization'
+      // (as you require)
+
+      //// If you want to set the version, you can do so:
+      // v: '3.26',
+    },
+
+    //// If you intend to programmatically custom event listener code
+    //// (e.g. `this.$refs.gmap.$on('zoom_changed', someFunc)`)
+    //// instead of going through Vue templates (e.g. `<GmapMap @zoom_changed="someFunc">`)
+    //// you might need to turn this on.
+    // autobindAllEvents: false,
+
+    //// If you want to manually install components, e.g.
+    //// import {GmapMarker} from 'vue2-google-maps/src/components/marker'
+    //// Vue.component('GmapMarker', GmapMarker)
+    //// then set installComponents to 'false'.
+    //// If you want to automatically install all the components this property must be set to 'true':
+    installComponents: true
+  })
+  
+  appOptions.vuetify = new Vuetify(opts);
+  
+  // Set default layout as a global component
+  Vue.component('Layout', DefaultLayout)
+}
+
+
+import Vue from 'vue'
+
---
+++ src/pages/Index.vue
@@ -0,0 +1,93 @@
+<template>
+  <Layout>
+<div id="example-2">
+
+      <button @click="show = !show">Toggle show</button>
+        <transition appear name="bounce">
+          <p v-if="show">Lorem ipsum dolor sit amet, consectetur adipiscing elit. Mauris facilisis enim libero, at lacinia diam fermentum id. Pellentesque habitant morbi tristique senectus et netus.</p>
+        </transition>
+      </div>
+      
+      <!-- Learn how to use images here: https://gridsome.org/docs/images -->
+      <!-- <g-image alt="Example image" src="~/favicon.png" width="135" /> -->
+
+      <!-- <h1>Hello, world!</h1> -->
+      <v-app>
+        <v-avatar
+        color="primary"
+        size="56"
+        >VJ</v-avatar>
+        <v-alert
+        color="green"
+        outlined
+        type="success"
+        >weee</v-alert>
+        <v-btn
+        color="primary"
+        elevation="2"
+        >go</v-btn>
+        <v-alert
+        color="orange"
+        type="error"
+        >asfasfsa</v-alert>
+
+        <v-menu transition="slide-x-transition">
+          <template v-slot:activator="{ on, attrs }">
+            <v-btn
+            color="primary"
+            class="ma-2"
+            v-bind="attrs"
+            v-on="on"
+            >
+            Slide X Transition
+          </v-btn>
+        </template>
+        <v-list>
+          <v-list-item
+          v-for="n in 5"
+          :key="n"
+          link
+          >
+          <v-list-item-title v-text="'Item ' + n"></v-list-item-title>
+        </v-list-item>
+      </v-list>
+    </v-menu>
+</v-app>
+</Layout>
+</template>
+
+<script>
+  export default {
+    metaInfo: {
+      title: 'Hello, world!'
+    },
+    data: function () {
+      return {
+        show: true
+      }
+    },
+  }
+</script>
+
+<style>
+  .home-links a {
+    margin-right: 1rem;
+  }
+  .bounce-enter-active {
+  animation: bounce-in .5s;
+}
+.bounce-leave-active {
+  animation: bounce-in .5s reverse;
+}
+@keyframes bounce-in {
+  0% {
+    transform: scale(0);
+  }
+  50% {
+    transform: scale(1.5);
+  }
+  100% {
+    transform: scale(1);
+  }
+}
+</style>
