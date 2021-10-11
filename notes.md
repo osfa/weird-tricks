0f29674a40526dde4e35e88bd0319dd1181ce400
jacob broms <jacob.broms@sanocore.se>
2021-10-11 16:56:10 +0200

    notes and markers

--- notes.md
+++ notes.md
@@ -1,80 +1,169 @@
+# key features
+
+- a search
+
+- page footer nav
+
+  - single weird button floating in center that takes you onwards?
+  - use keyboard to navigate, accessaibilty?
+
+- change map colors (set colorschemes?)
+
+- main about text?
+
+## features
+
+- randomize/animate icons on nav
+
 - random gan face in menu?
-- def nav sounds between...make a bakclog? random sound on stranstion, random sound on button press? emit events?
-- randomize image load? have stack of imgs?
-- page nav..
 
-- make like component sculputers with crypto humans part as well / choose memphis color scheme? / same for maps but less atuaredd?
-- emit oeb ilnk : opens archived link thing? all img hotlinked? have nice 404 thing thoought? hotlinked mage stuff?
-
-- incredibly complicated randomized connection between articles? in tree view? circle? nodes?
-- add and tile on top? some windows?
-- like a spam bot site? all the different links and menu items randomly select aiatcles aetc? weird article content in stringse etc?
-- emit evetns on like 20 diff types of components. spawn new one that pulls img / text / article / sound / vidoe / collage?
-
-- everything needs top happen on top of map? scrolls around on top?
-
-- random button actions do fif things? move around ranodmly on map? diff ui componetns popup and move you around to next node. global state stadck?
-
-so can close down?
-
-someimes just tons of mapp oonts spanwed.
-
-- need a serach?
-
-- all clicks emit map move evnt?
-
--- thinner map block and then overlay inside of that? fullscreen allow?
-
-- video / text whatever block below or hovering on top?
-
-- center evrything? or stack?
-
-- log and track all interactions for user?
-
-- just two pages : about main, tree of CTF content? jumbled?
-
-- rabdom elements with random icons in nav drawer. some favoite stuff that acutally maps?
-
-- really long inbox? what emails?
-
-- animate in text and things in menu? changes to clickbaity stuff
-
-- the entry into a world view, a sphere of links, an algoritumicl bubble
-
-- use keyboard to navigate, accessaibilty?
-
-- fake tweets? some gpt2?
+- def nav sounds between.. random sound on transition, random sound on button press? emit events?
 
 - binaural: https://github.com/yomansk8/binaural/blob/master/src/App.js
 
-- randomize icons?
+- clickbait text shift
 
----
+- random dropshadow on load?
+  - cloud overlays? on site? or map?
 
-landing page should be all mapp?
-randomly seed map with marekrs as you scroll?
+## map
 
-single weird button floating in center that takes you onwards?
+- landing page should be all map
 
-really need nav state in bottom somehow?
-
-animate icons in bottom nav on press?
-
-random dropshadow on load?
+- someimes just tons of mapp poiints spanwed.
+- color shift
+- virtual map overlays?
 
 - need map overlays? like heatmap but also transp pngs?
 
-- appear buttons on hover?
+- randomly seed map with marekrs as you scroll?
 
----
+- leylines draw raster of lines? empty markers?
 
-video
+  - https://www.pinterest.se/pin/298082069064797892/
+  - place markers along the lines?
 
-copyright free audio and EDL cuts?
+- more tile based? hover on tiles? so images that are squares on top? depending on zoom level?
 
-- humans svg gpt3?
+- make thing that bleeds into real lake? data lake to lake? to swamp? whatever?
 
----
+## general
+
+- like a spam bot site? all the different links and menu items randomly select aiatcles aetc? weird article content in stringse etc?
+
+- mix in some gpt3 headline stuff?
+
+- the entry into a world view, a sphere of links, an algoritumicl bubble
+
+- emit open link : opens archived link thing? all img hotlinked? have nice 404 thing thoought? hotlinked mage stuff?
+
+## idea seeds
+
+- log and track all interactions for user?
+
+# content
+
+- fake tweets? some gpt2?
+- flat gulag? mega collage?
+- weird terms of service Gtp?
+- foodora screens
+- bot restaruants locs? google cards?
+- amazon.mp3
+- analytics counters?
+  - sound of counters?
+- animal tracking stuff
+
+## video
+
+- copyright free audio and EDL cuts?
+- humans svg gpt3 : mangle vectors?
+- live stock data api
+
+## content formats
+
+- TTS stories and video clips?
+- article / headline / link / pdf / video / collage
+- javscript widget / animation
+- fiverr commissions about how it working on fiverr?
+
+- how to do chapters nicely?
+
+- gaugan stuff eco system stuff for terra? ecosystem gaugan blend..
+
+- stovckvid blend scell scenarios cartton effects?
+- adverserial vid?
+- slow down vids? manip other ways?
+
+  - remove faces?
+
+- EDL auto cut tech demos? that palantir styled thing? add audio clip?
+
+## overlays
+
+- ecosystem outlines
+
+- flat design, humans, corporate memphis
+- tiktok cheat codes
+
+- pure blockchain and network aehsethics stuff?
+
+- cable data
+- cloud pngs
+- virtual map tiles
+
+- dynamic searches or something?
+
+- map pins
+- rogue map tiles
+- tsnes
+
+- twitter climate videos (linked to location?)
+- robo dogs, tech videos, etc.
+
+- shitty mobile game ads?
+
+- auto collager blocks:
+
+  - data lakes
+  - gan charts
+  - stock market
+  - bitcoin ads
+  - yt thumb nails mud fish content? yt thumbs garbage
+  - stock market charts behaving weirdly
+
+- network topography animations and js shit? http://paperjs.org/examples/candy-crash/ toys.prototyp.se?
+
+- captchkas - these pictures all give off the icky vibe that comes from a lack of consent. Nobody in the photos expected to be photographed. These images are thus all inherently voyeuristic. Worse, because they’re also incredibly bland, the voyeurism feels all the more sordid and vapid. As Todd put it …
+
+- cookie prompts?
+
+- gpt3 article about algorithimic governance?
+
+## JS
+
+- https://www.createwithdata.com/gapminder-chartjs/
+- https://www.chartjs.org/docs/latest/samples/bar/vertical.html
+- https://inorganik.github.io/countUp.js/
+
+## fiverr
+
+-- fiverr essays on site? prmotps? scavenger hunt? you find and piece the little parts together?
+!!- reflect on the fiverr labor stuff? - give some sort of angles?
+
+# (typora) keywords
+
+- prediction
+- black box
+- bias
+- platos cave
+- surveillance
+- hyperreality
+- mapping and archives
+-
+- clouds/mining
+- cal id
+
+# lat lngs
 
 8.725 -72.901
 55.191 131.915
@@ -83,11 +172,3 @@ copyright free audio and EDL cuts?
 30.807 -11.996
 18.939 84.554
 -30.878 119.985
-
----
-
-GPT2 TOS?
-
-- flat gulag? mega collage?
-
-- bot content?
--- src/layouts/Main.vue
+++ src/layouts/Main.vue
@@ -4,18 +4,19 @@
       v-if="true"
       ref="mapRef"
       :options="{
-        zoomControl: false,
+        zoomControl: true,
         mapTypeControl: false,
-        scaleControl: false,
+        scaleControl: true,
         streetViewControl: false,
         rotateControl: false,
         fullscreenControl: false,
         disableDefaultUi: false,
-        scrollwheel: false,
+        scrollwheel: true,
+        styles: mapStyle.styles,
       }"
       :center="center"
       :zoom="zoom"
-      map-type-id="terrain"
+      :map-type-id="mapType"
       style="
         width: 100%;
         height: 100%;
@@ -31,9 +32,28 @@
         :position="m.position"
         :clickable="true"
         :draggable="true"
+        :icon="m.icon"
+        :label="m.label"
+        @click="markerClick(m)"
+      />
+
+      <gmap-custom-marker :marker="customMarker">
+        <img src="http://lorempixel.com/50/50/nature/" />
+        <span>WAZZZAP</span>
+      </gmap-custom-marker>
+
+      <GmapMarker
+        :key="index"
+        v-for="(m, index) in circleMarkers"
+        :position="m.position"
+        :clickable="true"
+        :draggable="true"
+        :icon="m.icon"
+        :label="m.label"
         @click="markerClick(m)"
       />
     </GmapMap>
+
     <slot />
   </div>
 </template>
@@ -59,18 +79,111 @@
 
 <script>
 import RegularNav from "~/components/nav/RegularNav.vue";
+import GmapCustomMarker from "vue2-gmap-custom-marker";
+
+const mapMarker = "http://lorempixel.com/100/100/nature/"; // require if assets
 
 export default {
   components: {
     RegularNav,
+    "gmap-custom-marker": GmapCustomMarker,
   },
   data: () => ({
-    zoom: 10,
+    zoom: 7,
+    mapType: "terrain",
+    mapStyle: {
+      // other properties...
+      styles: [
+        { elementType: "geometry", stylers: [{ color: "#242f3e" }] },
+        { elementType: "labels.text.stroke", stylers: [{ color: "#242f3e" }] },
+        { elementType: "labels.text.fill", stylers: [{ color: "#746855" }] },
+        {
+          featureType: "administrative.locality",
+          elementType: "labels.text.fill",
+          stylers: [{ color: "#d59563" }],
+        },
+        {
+          featureType: "poi",
+          elementType: "labels.text.fill",
+          stylers: [{ color: "#d59563" }],
+        },
+        {
+          featureType: "poi.park",
+          elementType: "geometry",
+          stylers: [{ color: "#263c3f" }],
+        },
+        {
+          featureType: "poi.park",
+          elementType: "labels.text.fill",
+          stylers: [{ color: "#6b9a76" }],
+        },
+        {
+          featureType: "road",
+          elementType: "geometry",
+          stylers: [{ color: "#38414e" }],
+        },
+        {
+          featureType: "road",
+          elementType: "geometry.stroke",
+          stylers: [{ color: "#212a37" }],
+        },
+        {
+          featureType: "road",
+          elementType: "labels.text.fill",
+          stylers: [{ color: "#9ca5b3" }],
+        },
+        {
+          featureType: "road.highway",
+          elementType: "geometry",
+          stylers: [{ color: "#746855" }],
+        },
+        {
+          featureType: "road.highway",
+          elementType: "geometry.stroke",
+          stylers: [{ color: "#1f2835" }],
+        },
+        {
+          featureType: "road.highway",
+          elementType: "labels.text.fill",
+          stylers: [{ color: "#f3d19c" }],
+        },
+        {
+          featureType: "transit",
+          elementType: "geometry",
+          stylers: [{ color: "#2f3948" }],
+        },
+        {
+          featureType: "transit.station",
+          elementType: "labels.text.fill",
+          stylers: [{ color: "#d59563" }],
+        },
+        {
+          featureType: "water",
+          elementType: "geometry",
+          stylers: [{ color: "#17263c" }],
+        },
+        {
+          featureType: "water",
+          elementType: "labels.text.fill",
+          stylers: [{ color: "#515c6d" }],
+        },
+        {
+          featureType: "water",
+          elementType: "labels.text.stroke",
+          stylers: [{ color: "#17263c" }],
+        },
+      ],
+    },
     markers: [
       {
         position: {
           lat: 15.0,
           lng: 15.0,
+        },
+        icon: {
+          url: mapMarker,
+          // size: { width: 60, height: 90, f: "px", b: "px" },
+          // scaledSize: { width: 30, height: 45, f: "px", b: "px" },
         },
         weight: 100,
       },
@@ -79,20 +192,69 @@ export default {
           lat: 14.0,
           lng: 13.0,
         },
+        // label: "Potato",
+
         weight: 50,
       },
     ],
+    circleMarkers: [],
+    customMarker: {
+      lat: 10,
+      lng: 10,
+    },
     center: { lat: 10, lng: 10 },
   }),
   async mounted() {
     this.$store.commit("setMainContent", this.$static.blogPosts.edges);
     await this.$gmapApiPromiseLazy();
+    const circleCoords = [
+      ...this.drawCircle(new google.maps.LatLng(10, 10), 100, 1),
+      ...this.drawCircle(new google.maps.LatLng(10, 10), 200, 1),
+    ];
+    this.circleMarkers = circleCoords.map((m) => {
+      return {
+        position: {
+          lat: m.lat(),
+          lng: m.lng(),
+        },
+      };
+    });
+    console.log(potato);
+    // this.circleMarkers =
   },
   methods: {
     markerClick(marker) {
       console.log("potato");
       this.center = marker.position;
       this.$emit("markerClicked");
+    },
+    drawCircle(point, radius, dir) {
+      console.log("drawcircle1");
+
+      var d2r = Math.PI / 180; // degrees to radians
+      var r2d = 180 / Math.PI; // radians to degrees
+      var earthsradius = 3963; // 3963 is the radius of the earth in miles
+
+      var points = 32;
+      // find the raidus in lat/lon
+      var rlat = (radius / earthsradius) * r2d;
+      var rlng = rlat / Math.cos(point.lat() * d2r);
+
+      var extp = [];
+      if (dir == 1) {
+        var start = 0;
+        var end = points + 1; // one extra here makes sure we connect the path
+      } else {
+        var start = points + 1;
+        var end = 0;
+      }
+      for (var i = start; dir == 1 ? i < end : i > end; i = i + dir) {
+        var theta = Math.PI * (i / (points / 2));
+        let ey = point.lng() + rlng * Math.cos(theta); // center a + radius x * cos(theta)
+        let ex = point.lat() + rlat * Math.sin(theta); // center b + radius y * sin(theta)
+        extp.push(new google.maps.LatLng(ex, ey));
+      }
+      return extp;
     },
   },
   watch: {
